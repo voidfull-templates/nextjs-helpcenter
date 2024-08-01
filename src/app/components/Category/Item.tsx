520e24b6ee31a5e59a8a547e6c028cc512b2a654
Yashu Mittal <mittalyashu77@gmail.com>
2024-08-01 16:26:53 +0530

    feat: show categories in homepage

---
+++ src/app/components/Category/Item.tsx
@@ -0,0 +1,49 @@
+import dayjs from "dayjs";
+import type { ActiveCategory } from "@voidfull/js-sdk";
+
+import { cn } from "@/app/utils";
+
+interface Props {
+	category: ActiveCategory;
+}
+
+export function CategoryItem({ category }: Props) {
+	return (
+		<a
+			className={cn(
+				"p-4 rounded-lg border border-gray-300",
+				"flex items-center justify-between",
+			)}
+			href={`/${category.id}-${category.slug}`}
+		>
+			<div
+				className={cn("rounded mr-4", "w-10 h-10", "flex-shrink-0")}
+				aria-hidden="true"
+			>
+				{category.icon ? (
+					<img
+						src={category.icon}
+						alt={category.name}
+						loading="lazy"
+						decoding="async"
+					/>
+				) : null}
+			</div>
+
+			<div className="w-full">
+				<div className={cn("text-lg font-semibold")}>{category.name}</div>
+
+				{category.description ? (
+					<p className="mt-2">{category.description}</p>
+				) : null}
+
+				{category.createdAt ? (
+					<time className={cn("text-xs text-gray-600", "mt-2")}>
+						Created at {dayjs(category.createdAt).format("MMMM DD, YYYY")}
+					</time>
+				) : null}
+				{/* <p className="text-gray-500">By Jake • {articles} articles</p> */}
+			</div>
+		</a>
+	);
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,32 @@
+import { Client as Voidfull } from "@/lib/Voidfull";
+import { cn } from "@/app/utils";
+
+import { ReferenceLinks } from "@/app/components/ReferenceLinks";
+import { CategoryItem } from "@/app/components/Category/Item";
+
+async function getCategories() {
+	const response = await Voidfull.sites.categories.list({
+		status: ["active"],
+	});
+	return response.categories;
+}
+
+export default async function Home() {
+	const data = await getCategories();
+
+	return (
+		<main className="mx-auto max-w-screen-lg px-4">
+			<section className={cn("my-10")}>
+				<ReferenceLinks />
+			</section>
+
+			{data?.length > 0 ? (
+				data?.map((category) => (
+					<CategoryItem key={category.id} category={category} />
+				))
+			) : (
+				<p className={cn("text-sm text-center")}>No categories</p>
+			)}
+		</main>
+	);
+}
