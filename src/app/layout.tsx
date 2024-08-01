0b03f0856623c3e77f8b63ad00fb128947e5caf5
Yashu Mittal <mittalyashu77@gmail.com>
2024-08-01 18:40:33 +0530

    feat: add Footer to global layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,37 @@
 import type { Metadata } from "next";
+import type { ReactNode } from "react";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Root as Header } from "./components/Header/Root";
 
-const inter = Inter({ subsets: ["latin"], weight: ["400", "500", "600"] });
+import { Root as Header } from "./components/Header/Root";
+import { Footer } from "@/app/components/Footer";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+});
 
 export const metadata: Metadata = {
-  title: "My Voidfull help center",
+  title: "My Voidfull Help Center",
 };
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/svg+xml" href="/voidfull.svg" />
+      </head>
+
       <body className={inter.className}>
         <Header />
 
         {children}
+
+        <Footer />
       </body>
     </html>
   );
